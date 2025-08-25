3dffbeaeaccc2776046a08d1afa22f2ce65d7621
nategoss <ngossiu@gmail.com>
2025-08-25 12:01:42 -0700

    Update

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from '../App'
+import '../styles/globals.css'
 
-  import { createRoot } from "react-dom/client";
-  import App from "./App.tsx";
-  import "./index.css";
+// Ensure the root element exists
+const rootElement = document.getElementById('root')
+if (!rootElement) {
+  throw new Error('Root element not found. Make sure there is a div with id="root" in your HTML.')
+}
 
-  createRoot(document.getElementById("root")!).render(<App />);
-  
+ReactDOM.createRoot(rootElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
